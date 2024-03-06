6551673257bb33f01d9979857634df490fcaae09
squoosh4 <triticalh1t@gmail.com>
2024-03-06 13:09:35 -0500

    weWF
    wefweqf

---
+++ project/src/components/yap.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+     
+        data:{
+
+        },
+        mounted: fucntoipm() {
+            this.fetchData;
+        }
+        methosds:{
+            fetchData: asunc func tiolm() {
+                try {
+                    const result = await fetch("https://data.cityofnewyork.us/resource/uip8-fykc.json");
+                }
+                catch (error) {
+                    console.log(error)
+
+                }
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
