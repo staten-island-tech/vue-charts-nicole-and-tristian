a32371d2d39f4caf9b4ad191aa4ad691fa7b30cb
nicolew1070 <145048184+nicolew1070@users.noreply.github.com>
2024-03-07 13:12:09 -0500

    .

---
+++ project/src/components/chart.vue
@@ -0,0 +1,18 @@
+<template>
+    <Bar
+    id = "mychart-id"
+    :options = "chartOptions"
+    :data = "chartData" 
+    />
+</template>
+
+<script>
+import {Bar} from 'vue-chart.js'
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
