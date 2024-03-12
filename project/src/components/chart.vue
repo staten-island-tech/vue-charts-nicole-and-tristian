05b4218789cb3550ee305286af923c71002a496a
nicolew1070 <145048184+nicolew1070@users.noreply.github.com>
2024-03-12 13:16:07 -0400

    /

--- project/src/components/chart.vue
+++ project/src/components/chart.vue
@@ -1,18 +1,21 @@
 <template>
-    <Bar
-    id = "mychart-id"
-    :options = "chartOptions"
-    :data = "chartData" 
-    />
+  <Pie :data="data" :options="options" />
 </template>
 
 <script>
-import {Bar} from 'vue-chart.js'
-    export default {
-        
+export const data = {
+  labels: ['Robbery', 'Felony Assault', 'Arson', 'Petit Larceny'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [40, 20, 80, 10]
     }
+  ]
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
 </script>
-
-<style lang="scss" scoped>
-
-</style>
