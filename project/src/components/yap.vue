64fa3e5b53486c91c71b3f60c0591c69dcb61205
PamiSamiK <PAmithis@nycstudents.net>
2024-03-12 13:11:19 -0400

    5ETRY5
    1 6QY

--- project/src/components/yap.vue
+++ project/src/components/yap.vue
@@ -1,33 +1,24 @@
-c<template>
-    <div>
-
-    </div>
+c
+<template>
+  <div></div>
 </template>
 
 <script>
-    export default {
-     
-        data:{
-
-        },
-        mounted: function () {
-            this.fetchData;
-        },
-        methods:{
-            fetchData: async function () {
-                try {
-                    const result = await fetch("https://data.cityofnewyork.us/resource/uip8-fykc.json");
-                }
-                catch (error) {
-                    console.log(error)
-
-                }
-            }
-        }
-
+export default {
+  data: {},
+  mounted: function () {
+    this.fetchData
+  },
+  methods: {
+    fetchData: async function () {
+      try {
+        const result = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+      } catch (error) {
+        console.log(error)
+      }
     }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,9 +1,69 @@
 <template>
-  <div></div>
+  <Bar :data="data" :options="options" />
 </template>
 
-<script>
-export default {}
-</script>
+<script lang="ts">
 
-<style scoped></style>
+import { createApp } from 'vue'
+import App from './src/App.vue'
+
+createApp(App).mount('#app')
+
+export const data = {
+  labels: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+    }
+  ]
+}
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
+
+
+
+
+import {
+
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+import * as chartConfig from './chartConfig.js'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Bar
+  },
+  data() {
+    return chartConfig
+  }
+}
+</script>
