06c2f87f0eb4b0e5fbbdc1c6705cb11a96563c81
squoosh4 <triticalh1t@gmail.com>
2024-03-19 13:12:25 -0400

    Update BarChart.vue

--- project/src/components/BarChart.vue
+++ project/src/components/BarChart.vue
@@ -12,6 +12,36 @@ import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, Li
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
+const api_data = ;
+const crimes = await ;
+let labels = [];
+ 
+// Count variable is used to add the
+// new unique value only once in the
+// outputArray.
+let count = 0;
+ 
+// Start variable is used to set true
+// if a repeated duplicate value is
+// encontered in the output array.
+let start = false;
+ 
+for (let j = 0; j < crimes.length; j++) {
+    for (let k = 0; k < outputArray.length; k++) {
+        if (Arr[j] == outputArray[k]) {
+            start = true;
+        }
+    }
+    count++;
+    if (count == 1 && start == false) {
+        outputArray.push(Arr[j]);
+    }
+    start = false;
+    count = 0;
+}
+ 
+console.log(outputArray);
+
 export default {
   name: 'BarChart',
   components: { Bar },
