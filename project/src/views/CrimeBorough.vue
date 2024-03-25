0d634be375956e214693fc8ba4aadad8ff3c65d8
nicolew1070 <145048184+nicolew1070@users.noreply.github.com>
2024-03-25 13:12:35 -0400

    yhtefd

---
+++ project/src/views/CrimeBorough.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>Crime Type Based on Borough</h1>
+    <div>
+  <!--     <Pie :data="data" :options="options" /> -->
+      <select name="" id="list">
+        <option value="">Brooklyn</option>
+        <option value="">Staten Island</option>
+        <option value="">Bronx</option>
+        <option value="">Queens</option>
+        <option value="">Manhattan</option>
+      </select>
+    </div>
+  </template>
+
+<!--   <script>
+  import { Pie } from 'vue-chartjs'
+  import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+  ChartJS.register(ArcElement, Tooltip, Legend)
+  
+  export default {
+    data: {},
+    mounted: function () {
+      this.fetchData
+    },
+    methods: {
+      fetchData: async function () {
+        try {
+          const result = await fetch('https://data.cityofnewyork.us/resource/uip8-fykc.json')
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+  }
+  </script> 
+  
+  <style scoped>
+</style> -->
