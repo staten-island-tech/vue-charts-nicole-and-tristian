85da48673eaafcd2b2bdcc25ad6bdde1bf3fc50f
nicolew88 <nicolew88@nycstudents.net>
2024-03-24 23:25:13 -0400

    ad

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,36 +1,82 @@
-<template>
-  <Pie :data="data" :options="options" />
-  <select name="" id="list">
-    <option value="">Brooklyn</option>
-    <option value="">Staten Island</option>
-    <option value="">Bronx</option>
-    <option value="">Queens</option>
-    <option value="">Manhattan</option>
-  </select>
-</template>
-
-<script>
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Pie } from 'vue-chartjs'
-import * as chartConfig from './components/chart.vue'
-
-ChartJS.register(ArcElement, Tooltip, Legend)
-
-export default {
-  name: 'App',
-  components: {
-    Pie
-  },
-  data() {
-    return chartConfig
-  }
-}
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/"></RouterLink>
+        <RouterLink to="/about"></RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
 <style scoped>
-#list {
-  padding: 0.25rem;
-  font-size: 1.1rem;
-  margin: 8rem auto;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
--- project/src/components/chart.vue
+++ project/src/components/chart.vue
@@ -1,7 +1,3 @@
-<template>
-  <Pie :data="data" :options="options" />
-</template>
-
 <script>
 export const data = {
   labels: ['Robbery', 'Felony Assault', 'Arson', 'Petit Larceny'],
@@ -16,12 +12,26 @@ export const data = {
 
 
 <template>
+  <h1>Crime Type Based on Borough</h1>
   <div>
-    <h1>Crime Based on Borough</h1>
+    <Pie :data="data" :options="options" />
+    <select name="" id="list">
+    <option value="">Brooklyn</option>
+    <option value="">Staten Island</option>
+    <option value="">Bronx</option>
+    <option value="">Queens</option>
+    <option value="">Manhattan</option>
+  </select>
   </div>
 </template>
 
 <script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+
+
 export default {
   data: {},
   mounted: function () {
