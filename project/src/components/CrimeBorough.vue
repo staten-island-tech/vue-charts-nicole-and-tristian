a59c2bb1f5f86dfc97cef94ebc947637e0f71a2c
nicolew1070 <145048184+nicolew1070@users.noreply.github.com>
2024-03-25 12:42:23 -0400

    bnm

--- project/src/components/CrimeBorough.vue
+++ project/src/components/CrimeBorough.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 export const data = {
   labels: ['Robbery', 'Felony Assault', 'Arson', 'Petit Larceny'],
   datasets: [
@@ -8,29 +8,26 @@ export const data = {
     }
   ]
 }
-</script>
-
+</script> -->
 
 <template>
   <h1>Crime Type Based on Borough</h1>
   <div>
     <Pie :data="data" :options="options" />
     <select name="" id="list">
-    <option value="">Brooklyn</option>
-    <option value="">Staten Island</option>
-    <option value="">Bronx</option>
-    <option value="">Queens</option>
-    <option value="">Manhattan</option>
-  </select>
+      <option value="">Brooklyn</option>
+      <option value="">Staten Island</option>
+      <option value="">Bronx</option>
+      <option value="">Queens</option>
+      <option value="">Manhattan</option>
+    </select>
   </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
+import { Pie } from 'vue-chartjs'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 ChartJS.register(ArcElement, Tooltip, Legend)
-
-
 
 export default {
   data: {},
@@ -49,5 +46,6 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
 
+</style>
