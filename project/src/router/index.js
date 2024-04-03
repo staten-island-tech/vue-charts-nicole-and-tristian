6d98f75df16cf78141090650ae183a2584f6f368
nicolew88 <nicolew88@nycstudents.net>
2024-04-02 21:45:01 -0400

    af

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/CrimeBorough.vue'
-
+import HomeView from '../views/PieChart.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
