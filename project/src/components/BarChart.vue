b3009a0bc8dee1a6b813d91e35370f0b74759d50
squoosh4 <triticalh1t@gmail.com>
2024-04-03 13:12:27 -0400

    fdy
    dyt

--- project/src/components/BarChart.vue
+++ project/src/components/BarChart.vue
@@ -57,13 +57,46 @@ export default {
         labels: ["Winter", "Summer", "Spring", "Fall"],
         datasets: [
           {
-            backgroundColor: ["#0000ff", "ff0000", "00ff00", "#ff8800"],
+            backgroundColor: ["#0000ff", "#ff0000", "#00ff00", "#ff8800"],
             data: seasonalCrimeRateArray
             //data: [winterCrimePercentage, springCrimePercentage, summerCrimePercentage, autumnCrimePercentage] 
           }
         ]
       }
     }
-  }
-}
+  },
+  mounted: {
+    this.buildChartData();
+  },
+  methods: {
+    function buildChartData(){
+      const rawData = await fetch("https://data.cityofnewyork.us/resource/uip8-fykc.json");
+      const apiData = await rawData.json();
+      console.log(rawData);
+      console.log(apiData);
+
+      let winterCrimeCount, springCrimeCount, summerCrimeCount, autumnCrimeCount = 0;
+
+      apiData.forEach((element) =>
+      {
+        if (element.arrest_date[6] === 1) {
+          autumnCrimeCount++;
+        } else if (element.arrest_date[7] === 1 | 2 | 3) {
+          winterCrimeCount++;
+        } else if (element.arrest_date[7] === 4 | 5 | 6) {
+          springCrimeCount++;
+        } else if (element.arrest_date[7] === 7 | 8 | 9) {
+        summerCrimeCount++;
+        }
+      }
+      );
+
+      let winterCrimePercentage = (winterCrimeCount / ( winterCrimeCount + springCrimeCount + summerCrimeCount + autumnCrimeCount)) * 100;
+      let springCrimePercentage = (springCrimeCount / ( winterCrimeCount + springCrimeCount + summerCrimeCount + autumnCrimeCount)) * 100;
+      let summerCrimePercentage = (summerCrimeCount / ( winterCrimeCount + springCrimeCount + summerCrimeCount + autumnCrimeCount)) * 100;
+      let autumnCrimePercentage = (autumnCrimeCount / ( winterCrimeCount + springCrimeCount + summerCrimeCount + autumnCrimeCount)) * 100; 
+
+      const seasonalCrimeRateArray = [winterCrimePercentage, springCrimePercentage, summerCrimePercentage, autumnCrimePercentage];
+      return seasonalCrimeRateArray;
+  }}}
 </script>
